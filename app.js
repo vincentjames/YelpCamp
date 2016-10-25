8f02161573fb7912997b69606d58b1c374559b3b
Vincent Bellittera <bellaproducts78@gmail.com>
2016-10-25 17:04:11 -0400

    Added edit comment page

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 var express         = require("express"),
-	app             = express(),
-	bodyParser      = require("body-parser"),
-	mongoose        = require("mongoose"),
+	  app             = express(),
+	  bodyParser      = require("body-parser"),
+	  mongoose        = require("mongoose"),
     passport        = require("passport"),
     LocalStrategy   = require("passport-local"),
-	Campground      = require("./models/campground"),
-	Comment         = require("./models/comment"),
+    methodOverride  = require("method-override"),
+	  Campground      = require("./models/campground"),
+	  Comment         = require("./models/comment"),
     User            = require("./models/user"),
-	seedDB          = require("./seeds");
+	  seedDB          = require("./seeds");
 
 // Requiring Routes
 var commentRoutes   = require("./routes/comments"),
     campgroundRoutes= require("./routes/campgrounds"),
-    indexRoutes      = require("./routes/index");
+    indexRoutes     = require("./routes/index");
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 // seedDB();  // seed the database
 
 // PASSPORT CONFIGURATION
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,25 +15,30 @@ router.get("/", function(req, res){
 });
 
 //CREATE - add new campground to DB
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = {name: name, image: image, description: desc};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: desc, author: author};
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err);
         } else {
             //redirect back to campgrounds page
+            console.log(newlyCreated);
             res.redirect("/campgrounds");
         }
     });
 });
 
 //NEW - show form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
    res.render("campgrounds/new"); 
 });
 
@@ -50,5 +55,38 @@ router.get("/:id", function(req, res){
         }
     });
 });
+
+// EDIT CAMPGROUND ROUTE
+router.get("/:id/edit", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if(err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.render("campgrounds/edit", {campground: foundCampground});     
+        }
+    });
+});
+
+router.put("/:id", function(req, res) {
+    // find and update the correct campground
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
+        if(err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id)
+        }
+    });
+});
+
+// UPDATE CAMPGROUND ROUTE
+
+
+// Middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
