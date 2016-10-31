7c6eba8ff469448587a2886b361433d8d1c93b89
Vincent Bellittera <bellaproducts78@gmail.com>
2016-10-31 13:03:26 -0400

    Added update and edit comment route and page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -65,7 +65,7 @@ router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
 
 
 // UPDATE CAMPGROUND ROUTE
-router.put("/:id", function(req, res) {
+router.put("/:id", checkCampgroundOwnership, function(req, res) {
     // find and update the correct campground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
         if(err) {
@@ -77,7 +77,7 @@ router.put("/:id", function(req, res) {
 });
 
 // DESTROY CAMPGROUND
-router.delete("/:id", function(req, res) {
+router.delete("/:id", checkCampgroundOwnership, function(req, res) {
     Campground.findByIdAndRemove(req.params.id, function(err) {
         if(err) {
             res.redirect("/campgrounds");
